d33fd97eab1bbc451ec7366b0eb4560cd33ab628
EstanislaoRon <eron@udesa.edu.ar>
2020-06-05 16:39:42 -0300

    Artist js y html

--- js/artists.js
+++ js/artists.js
@@ -2,37 +2,34 @@ window.addEventListener("load", function() {
 
     let queryString = new URLSearchParams (location.search);
     let codigoArtists = queryString.get()
-    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists")
-    .then(
-        function(respuesta) {
-            return respuesta.json();            
-        }
-    )
-    .then(
-        function(informacion) {
-            let artistasPopulares = informacion.data;
 
-            for (let index = 0; index < 10; index++) {
-                console.log(Artista)
-                const Artista = artistasPopulares[index];
-                let img = Artista.picture;
-                
-                
-                let title = data.title;
-                let id = data.id;
-
-                let htmlArtists = 
-                `
-                <section class="song">
-                <p class="nums">1</p>
-                <a href="` + id + `"><img class="topsART" src="` + picture + `" alt="">
-                <p class="name"> `+ name +` </p></a>
-                </section>
-                `
-                
-                
-                document.querySelector(".listadoArtistsPopulares").innerHTML += htmlArtists
+        fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+        .then(
+            function(respuesta) {
+                return respuesta.json();            
             }
-        } )
-            
-        })
+        )
+        .then(
+            function(informacion) {
+                console.log (informacion)
+                let artistasPopulares = informacion.artists.data;
+    
+                for (let index = 0; index <= 9; index++) {
+    
+                    const cadaArtists = artistasPopulares[index];
+                    console.log (cadaArtists)
+                    let img = cadaArtists;
+                    let title = cadaArtists.title;
+    
+                    let htmlNuevoArtists =
+                    `
+                    <section class="song">
+                    <p class="nums"> 1 </p>
+                    <a href="Infoartistas.html"> <img class="topsART" src= "`+ img + `" alt="">
+                    <p class="name">` + title + `</p></a>
+                    </section>
+                    `  
+                    document.querySelector(".ref2").innerHTML += htmlNuevoArtists
+                }
+            })  
+            })
