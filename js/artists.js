bd383a5e4b491febc53c1a430bb83a47e5c84572
EstanislaoRon <eron@udesa.edu.ar>
2020-06-05 17:40:35 -0300

    Me falta solo arreglar el titulo y las fotos, pero la base del js esta

--- js/artists.js
+++ js/artists.js
@@ -1,9 +1,6 @@
 window.addEventListener("load", function() {
-
-    let queryString = new URLSearchParams (location.search);
-    let codigoArtists = queryString.get()
-
-        fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
+console.log ("hola")
+        fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0/artists")
         .then(
             function(respuesta) {
                 return respuesta.json();            
@@ -12,21 +9,22 @@ window.addEventListener("load", function() {
         .then(
             function(informacion) {
                 console.log (informacion)
-                let artistasPopulares = informacion.artists.data;
+                let artistasPopulares = informacion.data;
     
-                for (let index = 0; index <= 9; index++) {
+                for (let index = 0; index <= 10; index++) {
     
-                    const cadaArtists = artistasPopulares[index];
+                    const cadaArtists = artistasPopulares[index].name;
                     console.log (cadaArtists)
                     let img = cadaArtists;
                     let title = cadaArtists.title;
+                    let posision =  cadaArtists.position;
     
                     let htmlNuevoArtists =
                     `
                     <section class="song">
-                    <p class="nums"> 1 </p>
-                    <a href="Infoartistas.html"> <img class="topsART" src= "`+ img + `" alt="">
-                    <p class="name">` + title + `</p></a>
+                    <p class="nums"> </p>
+                    <a href="Infoartistas.html"> <img class="topsART" src= "`+ data.picture + `" alt="">
+                    <p class="name">` + data.title + `</p></a>
                     </section>
                     `  
                     document.querySelector(".ref2").innerHTML += htmlNuevoArtists
