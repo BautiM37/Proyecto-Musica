4f4f91f35b24f06393894a3c3de83481e06e5962
Bauti Migliore <bauti@bautis-air.lan>
2020-06-13 16:01:53 -0300

    infoGenres vinculado atr

--- js/genre.js
+++ js/genre.js
@@ -21,7 +21,7 @@ window.addEventListener("load", function() {
 
                 let htmlNuevoGenre =`
                 <section class="genre">
-                <a href="infoGenres.html?idGenre= ` + id + `"><p class="nums"> - </p>
+                <a href="infoGenre.html?idGenre= ` + id + `"><p class="nums"> - </p>
                 <img class="tops" src="` + img + `" alt="">
                 <p class="name">` + title + `</p></a>
                 </section>   
